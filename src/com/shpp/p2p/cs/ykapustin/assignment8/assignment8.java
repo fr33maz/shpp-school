7994b0789ad162d1faf358bd70418713a1e43e21
monte <montesumma07@gmail.com>
2022-08-22 22:12:00 +0300

    22.10 constants was added and used to avoid magic numbers

--- src/com/shpp/p2p/cs/ykapustin/assignment8/assignment8.java
+++ src/com/shpp/p2p/cs/ykapustin/assignment8/assignment8.java
@@ -8,22 +8,29 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 public class assignment8 extends WindowProgram {
+    private final int N_RECTANGLES = 10;
+    private final int BOX_SIZE = 10;
+    private final int SPACE_BETWEEN_BOXES = 5;
+    private final int WIERD_CORRECTION = 12;
+    private final int APPLICATION_HEIGHT = 250;
+    private final int APPLICATION_WIDTH = 300;
+
     private final ArrayList<GObject> rectangle = new ArrayList<>();
-    private final boolean[] array = new boolean[10];
+    private final boolean[] element = new boolean[N_RECTANGLES];
     private final int[] direction = {3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
 
 
     public void run() {
         addMouseListeners();
-        setSize(300, 250);
+        setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
         createTenRectangles();
 
         while (true) {
             for (int i = 0; i < rectangle.size(); i++) {
-                if (array[i]) {
+                if (element[i]) {
                     rectangle.get(i).move(direction[i], 0);
                 }
-                if (!(rectangle.get(i).getX() < getWidth() - 12 && rectangle.get(i).getX() > 0)) {
+                if (!(rectangle.get(i).getX() < getWidth() - WIERD_CORRECTION && rectangle.get(i).getX() > 0)) {
                     direction[i] *= -1;
                 }
             }
@@ -34,20 +41,20 @@ public class assignment8 extends WindowProgram {
 
     private void createTenRectangles() {
         GRect gRect;
-        for (int i = 0; i < 10; i++) {
-            gRect = new GRect(getWidth() / 2.0, i * 15, 10, 10);
+        for (int i = 0; i < N_RECTANGLES; i++) {
+            gRect = new GRect(getWidth() / 2.0, i * (BOX_SIZE + SPACE_BETWEEN_BOXES), BOX_SIZE, BOX_SIZE);
             gRect.setFilled(true);
             rectangle.add(gRect);
         }
-        for (GObject ract : rectangle) {
-            add(ract);
+        for (GObject rect : rectangle) {
+            add(rect);
         }
     }
 
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        int element = e.getY() / 15;
+        int element = e.getY() / (BOX_SIZE + SPACE_BETWEEN_BOXES);
         arrayModifier(element);
 
     }
@@ -55,15 +62,15 @@ public class assignment8 extends WindowProgram {
     private void arrayModifier(int element) {
         if (element == 0) {
             for (int i = element; i <= element + 1; i++) {
-                array[i] = !array[i];
+                this.element[i] = !this.element[i];
             }
         } else if (element == 9) {
             for (int i = element - 1; i <= element; i++) {
-                array[i] = !array[i];
+                this.element[i] = !this.element[i];
             }
         } else {
             for (int i = element - 1; i <= element + 1; i++) {
-                array[i] = !array[i];
+                this.element[i] = !this.element[i];
             }
         }
     }
