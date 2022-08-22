43eafde5218dffed163f8417ff5fbc0314471f9f
fr33maz <63881229+fr33maz@users.noreply.github.com>
2022-08-22 17:30:20 +0300

    Delete src/com/shpp/p2p/cs/ykapustin/assignment8 directory

--- src/com/shpp/p2p/cs/ykapustin/assignment8/Main.java
+++
@@ -1,8 +0,0 @@
-package com.shpp.p2p.cs.ykapustin.assignment8;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("I'm about to start assignment8 at 17.30");
-    }
-}
-
