191f99886b4c4747b2e28a4a364c399fe3803252
monte <montesumma07@gmail.com>
2022-10-02 21:31:04 +0200

    Task of assignment 9 is to create save video from canvas which runs animation.

---
+++ src/com/shpp/p2p/cs/ykapustin/assignment9/VideoMaker.java
@@ -0,0 +1,55 @@
+package com.shpp.p2p.cs.ykapustin.assignment9;
+
+
+import org.jcodec.api.awt.AWTSequenceEncoder;
+import org.jcodec.common.io.FileChannelWrapper;
+import org.jcodec.common.io.NIOUtils;
+import org.jcodec.common.model.Rational;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class VideoMaker {
+
+    public static void main(String[] args) throws IOException {
+        FileChannelWrapper
+                out = NIOUtils.writableFileChannel("C:\\Users\\monte\\shpp\\folderForVideo\\video.mp4");
+        ArrayList<BufferedImage> images = fillArrayList();
+        AWTSequenceEncoder encoder = new AWTSequenceEncoder(out, Rational.R(50, 1));
+        for (Image image : images) {
+            BufferedImage scaledImage = scaledImage(image);
+            encoder.encodeImage(scaledImage);
+        }
+        encoder.finish();
+    }
+
+    private static BufferedImage scaledImage(Image image) {
+        Image scaled = image.getScaledInstance(760, 480, Image.SCALE_SMOOTH);
+        BufferedImage bf = new BufferedImage(scaled.getWidth(null), scaled.getHeight(null), BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bf.createGraphics();
+        g2d.drawImage(image, 0, 0, null);
+        g2d.dispose();
+        return bf;
+    }
+
+    private static ArrayList<BufferedImage> fillArrayList() {
+        ArrayList<BufferedImage> images = new ArrayList<>();
+        int number = 0;
+        while (true) {
+            String path = "C:\\Users\\monte\\shpp\\snapshots\\";
+            number++;
+            path += number + ".jpeg";
+            try {
+                BufferedImage image = ImageIO.read(new File(path));
+                images.add(image);
+            } catch (IOException e) {
+                break;
+            }
+        }
+        return images;
+    }
+}
